9eae4519d18c64c5e61e7426e2bfb97b0a68963c
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-02-04 08:49:22 -0500

    Added all variable to server.js as well as the the app.use needed for project completion

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Now listening at http://localhost:${PORT}`)
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
