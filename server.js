3a859415a731d4a636576b3fe73ac8d949ba97bf
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-02-05 09:43:25 -0500

    Added more functionality based off of notes from class.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+let notes = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,4 +22,27 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    res.json(notes);
+});
+
+app.put('/api/notes/:id', (req, res) => {
+    let noteId = req.params.id;
+    let updatedNote = req.body;
+    notes[noteId] = updatedNote;
+    res.json(notes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let noteId = req.params.id;
+    notes.splice(noteId, 1);
+    res.json(notes);
 });
