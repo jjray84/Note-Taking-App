e24f67e4b4e50f26b2d3e469933a2ccceb9fbb26
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-02-05 09:15:08 -0500

    continued with the minimum required code for the app to work.

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.listen(PORT, () => {
     console.log(`Now listening at http://localhost:${PORT}`)
 });
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
